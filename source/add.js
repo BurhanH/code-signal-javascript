78f6ddfc7875be5c04a7aefb4e65880b9a142805
Baur Urazalinov <BurhanH@users.noreply.github.com>
2020-06-12 13:05:40 -0500

    Create add.js
    Added fist sample

---
+++ source/add.js
@@ -0,0 +1,11 @@
+// https://app.codesignal.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa
+// Write a function that returns the sum of two numbers.
+
+function add(param1, param2) {
+    if (parseInt(param1) | parseInt(param2)) {
+        if (-1000 <= param1 <= 1000 | -1000 <= param2 <= 1000) {
+            return param1 + param2;
+        }
+    }
+    return 0;
+}
