49eddeb862585580e77ec873322781e4416e89bf
Baur Urazalinov <BurhanH@users.noreply.github.com>
2020-06-12 13:18:35 -0600

    Update addTest.js (#9)
    * Update addTest.js
    
    * Update addTest.js
    
    typo fix
    
    * Update add.js
    
    Fixed add function

--- source/add.js
+++ source/add.js
@@ -2,12 +2,16 @@
 // Write a function that returns the sum of two numbers.
 
 function add(param1, param2) {
-    if (parseInt(param1) | parseInt(param2)) {
-        if (-1000 <= param1 <= 1000 | -1000 <= param2 <= 1000) {
-            return param1 + param2;
-        }
-    }
+    if (typeof param1 === 'number' && (param1 % 1) === 0) {
+        if (typeof param2 === 'number' && (param2 % 1) === 0) {
+            if (-1000 <= param1 && param1 <= 1000) {
+                if (-1000 <= param2 && param2 <= 1000) {
+                    return param1 + param2;
+                };
+            };
+        };
+    };
     return 0;
-};
+}
 
 exports.add = add;
